6356f8d2a0ee55ee2b5a0967004c2292f5662b55
cZar Void <notczarvoid@gmail.com>
2024-06-01 11:25:47 -0400

    test

--- script.lua
+++ script.lua
@@ -1,58 +1,141 @@
--- Required libraries
-local http = require("socket.http")
-local ltn12 = require("ltn12")
-local htmlparser = require("htmlparser")
+print(window.query)
 
--- Function to fetch search results from Google
-local function fetch_search_results(query)
-    local search_url = "https://www.google.com/search?q=" .. query
-    local response = {}
-    
-    http.request{
-        url = search_url,
-        sink = ltn12.sink.table(response)
-    }
+local publish_domain = get("publish-input-domain")
+local publish_tld = get("publish-input-tld")
+local publish_ip = get("publish-input-ip")
+local publish_done = get("done-1")
 
-    return table.concat(response)
-end
+local update_key = get("update-input-key")
+local update_ip = get("update-input-ip")
+local update_done = get("done-2")
 
--- Function to parse search results and extract titles and links
-local function parse_search_results(html)
-    local tree = htmlparser.parse(html)
-    local results = {}
+local delete_key = get("delete-input-key")
+local delete_done = get("done-3")
 
-    -- Assuming results are in <h3> tags with class "LC20lb"
-    for _, node in ipairs(tree:select("h3.LC20lb")) do
-        local title = node:getcontent()
-        local link_node = node:parent()
-        if link_node.tag == "a" then
-            local link = link_node:getattribute("href")
-            table.insert(results, {title = title, link = link})
-        end
-    end
+local result = get("result")
+local img = get("image")
+local captcha_input = get("captcha_input")
 
-    return results
-end
-
--- Function to update HTML content with search results
-local function update_html_with_results(results)
-    local results_html = ""
-    for _, result in ipairs(results) do
-        results_html = results_html .. string.format(
-            '<li><a href="%s">%s</a></li>',
-            result.link,
-            result.title
-        )
-    end
-
-    local results_div = get("results")
-    results_div.set_content('<ul>' .. results_html .. '</ul>')
-end
-
--- Main coroutine to fetch and display search results
 coroutine.wrap(function()
-    local query = "Lua programming"
-    local html = fetch_search_results(query)
-    local results = parse_search_results(html)
-    update_html_with_results(results)
+    local res = fetch({
+        url = "http://api.buss.lol/tlds",
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" },
+    })
+
+    local tld_list = table.concat(res, ", ")
+    get("tlds").set_content("Available TLDs: " .. tld_list)
 end)()
+
+function fetch_dns(captcha)
+    local body = "{"
+    .. '"tld": "'
+    .. publish_tld.get_content()
+    .. '", '
+    .. '"name": "'
+    .. publish_domain.get_content()
+    .. '", '
+    .. '"ip": "'
+    .. publish_ip.get_content()
+    .. '"'
+
+    if captcha then
+        body = body .. ', "captcha": "' .. captcha_input.get_content() .. '" }'
+    else
+        body = body .. "}"
+    end
+
+    print(body)
+    local res = fetch({
+        url = "https://api.buss.lol/domain",
+        method = "POST",
+        headers = { ["Content-Type"] = "application/json" },
+        body = body,
+    })
+   
+    return res
+end
+
+captcha_input.on_submit(function()
+    local res = fetch_dns(true);
+
+    print(res)
+    if res["secret_key"] then
+        result.set_content("Congrats! Your key: " .. res["secret_key"] .. "\nPLEASE SAVE IT.")
+    end
+end)
+
+publish_done.on_click(function()
+    local res = fetch_dns(false)
+
+    if res and res.status then
+        if res.status == 202 then
+            img.set_source('http://api.buss.lol/captcha-images/' .. res.content .. '.jpg')
+        else
+            result.set_content(res.status .. ": " .. res.content)
+        end
+    else
+        result.set_content("Failed due to unknown error.")
+    end
+end)
+
+update_done.on_click(function()
+    local body = "{"
+        .. '"ip": "'
+        .. update_ip.get_content()
+        .. '"'
+        .. "}"
+
+    local res = fetch({
+        url = "http://api.buss.lol/domain/" .. update_key.get_content(),
+        method = "PUT",
+        headers = { ["Content-Type"] = "application/json" },
+        body = body,
+    })
+
+    print(res)
+
+    if res and res.status then
+        if res.status == 429 then
+            result.set_content("Failed due to ratelimit.")
+        elseif res.status == 404 then
+            result.set_content("Failed due to: domain not found.")
+        elseif res.status == 400 then
+            result.set_content("Failed due to: invalid body.\nMake sure all fields are completed")
+        elseif res.status == 200 then
+            result.set_content("Success!")
+        else
+            result.set_content("Failed due to error: " .. res.status)
+        end
+    elseif res and res.ip then
+        result.set_content("Success!")
+    else
+        result.set_content("Failed due to unknown error.")
+    end
+end)
+
+delete_done.on_click(function()
+    local res = fetch({
+        url = "http://api.buss.lol/domain/" .. delete_key.get_content(),
+        method = "DELETE",
+        headers = { ["Content-Type"] = "application/json" },
+    })
+
+    print(res)
+
+    if res and res.status then
+        if res.status == 429 then
+            result.set_content("Failed due to ratelimit.")
+        elseif res.status == 404 then
+            result.set_content("Failed due to: domain not found.")
+        elseif res.status == 200 then
+            result.set_content("Success!")
+        else
+            result.set_content("Failed due to error: " .. res.status)
+        end
+    elseif res and res.secret_key then
+        result.set_content("Success!")
+    else
+        result.set_content("Failed due to unknown error.")
+    end
+end)
